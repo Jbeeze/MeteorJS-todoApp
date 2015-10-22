a5cc24875f68cb91c6608731a2b792078ea5046b
Jared Billig <jared.billig@gmail.com>
2015-10-22 16:35:25 -0400

    initial commit of todo app

---
+++ todos.js
@@ -0,0 +1,45 @@
+Todos = new Mongo.Collection('todos');
+
+if (Meteor.isClient) {
+  // Template Helpers
+  Template.main.helpers({
+    todos: function(){
+      return Todos.find({}, {sort: {createdAt: -1}}); //sorts it by latest todo first
+    }
+  });
+
+  Template.main.events({
+    
+    "submit .new-todo": function(event){
+      var text = event.target.text.value;
+      console.log(text);
+
+      Todos.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clear Form
+      event.target.text.value = '';
+
+      // Prevent Submit
+      return false;
+    },
+    
+    "click .toggle-checked": function(){
+      Todos.update(this._id, {$set: {checked: ! this.checked}});
+    },
+
+    "click .delete-todo": function(){
+      if(confirm('Are You Sure?')){
+        Todos.remove(this._id);
+      }
+    }
+  });
+
+}
+
+
+if (Meteor.isServer) {
+
+}
