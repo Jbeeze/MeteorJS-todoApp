f9895d75e6ded74797b02b67a6b6d198388daa45
Jared Billig <jared.billig@gmail.com>
2015-10-22 17:02:05 -0400

    added addTodo, deleteTodo, and setChecked methods to Meteor for more security

--- todos.js
+++ todos.js
@@ -14,12 +14,7 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
       console.log(text);
 
-      Todos.insert({
-        text: text,
-        createdAt: new Date(),
-        userID: Meteor.userId(),
-        username: Meteor.user().username
-      });
+      Meteor.call('addTodo', text);
 
       // Clear Form
       event.target.text.value = '';
@@ -29,12 +24,12 @@ if (Meteor.isClient) {
     },
     
     "click .toggle-checked": function(){
-      Todos.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call('setChecked', this._id, !this.checked);
     },
 
     "click .delete-todo": function(){
       if(confirm('Are You Sure?')){
-        Todos.remove(this._id);
+        Meteor.call('deleteTodo', this._id);
       }
     }
   });
@@ -45,7 +40,31 @@ if (Meteor.isClient) {
 
 }
 
-
 if (Meteor.isServer) {
 
 }
+
+//Meteor Methods
+Meteor.methods({
+  
+  addTodo: function(text){
+    if(!Meteor.userId()){
+      throw new Meteor.Error('not-authorized')
+    }
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  
+  deleteTodo: function(todoId){
+    Todos.remove(todoId);
+  },
+
+  setChecked: function(todoId, setChecked){
+    Todos.update(todoId, {$set: {checked: setChecked}});
+  }
+
+});
